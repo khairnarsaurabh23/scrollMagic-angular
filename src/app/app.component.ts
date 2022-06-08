52e78f147415754b415674879498a1b7d999b359
Khairnar Saurabh <srkhairnar23@gmail.com>
2022-06-08 13:56:26 +0530

    scrollMagic with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ScrollMagic} from './../../node_modules/@types/scrollmagic';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  public A:number=0;
+  public B:number=0;
+  public C:number=0;
+
+  contructor(){
+  }
+
+  ngOnInit(){
+
+  }
+
   title = 'sm-test';
 }
